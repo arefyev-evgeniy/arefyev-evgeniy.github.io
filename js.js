390e1668702dae6a8acc9cd02dc6302ba8c2a916
Евгений Арефьев <111676852+arefyev-evgeniy@users.noreply.github.com>
2022-10-31 22:58:02 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+/**/
+const button = document.querySelector('button');
+
+/* */
+button.addEventListener('click', function() {
+	alert('Главное не всё знать, а уметь гуглить и быстро находить информацию)')
+})

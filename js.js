1a17204a690c7f8fd042439193263f56e444cb57
Евгений Арефьев <111676852+arefyev-evgeniy@users.noreply.github.com>
2022-10-31 22:57:35 +0500

    Delete js.js

--- js.js
+++
@@ -1,7 +0,0 @@
-/**/
-const button = document.querySelector('button');
-
-/* */Главное не всё знать, а уметь гуглить и быстро находить информацию
-button.addEventListener('click', function() {
-	alert('Главное не всё знать, а уметь гуглить и быстро находить информацию')
-})
